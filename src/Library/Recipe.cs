0ee08f93fceac8e3e04cdcf34fe0f665448f9530
ClaraPlateiro <clara.plateiro@correo.ucu.edu.uy>
2023-11-15 09:40:38 -0300

    Actualización

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -16,6 +16,8 @@ namespace Full_GRASP_And_SOLID
 
         public Product FinalProduct { get; set; }
 
+        public bool Cooked { get; private set; }
+
         // Agregado por Creator
         public void AddStep(Product input, double quantity, Equipment equipment, int time)
         {
@@ -62,5 +64,41 @@ namespace Full_GRASP_And_SOLID
 
             return result;
         }
+
+        public int GetCookTime()
+        {
+            int finalTime = 0;
+
+            foreach (BaseStep step in this.steps)
+            {
+                finalTime = finalTime + step.Time;
+            }
+            return finalTime;
+        }
+
+        public void Cook()
+        {
+            Cooked = false;
+            int finalTime = GetCookTime();
+
+            RecipeAdapter recipeAdapter = new RecipeAdapter(this);
+            CountdownTimer countdownTimer = new CountdownTimer();
+            countdownTimer.Register(finalTime, recipeAdapter);
+        }
+
+        public class RecipeAdapter : TimerClient
+        {
+            private Recipe recipe;
+            public RecipeAdapter(Recipe recipe)
+            {
+                this.recipe = recipe;
+            }
+            public void TimeOut()
+            {
+                this.recipe.Cooked = true;
+            }
+
+        }
+
     }
 }
